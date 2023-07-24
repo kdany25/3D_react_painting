f0079505be7e9b81450157bffca18317fbe1b796
kdany25 <kabadany25@gmail.com>
2023-07-24 09:33:51 +0200

    folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <></>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
 }
 
-export default App;
+export default App
